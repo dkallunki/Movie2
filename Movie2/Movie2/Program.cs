4993c152301dce7485b1bd52a887c5a5a95c2c90
Dana Kallunki <dana.kallunki@gmail.com>
2016-02-15 14:53:12 -0500

    Version 1: simple user entry, and echo of user input, do-while loop executes 3 times. Mirror of movie.cpp

---
+++ Movie2/Movie2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = " ";
+            int movieYear = 1900;
+            string movieRating = " ";
+            int counter=0;
+            do
+            {
+
+                // asks user to enter a movie name, and echo's response
+                System.Console.WriteLine("Please enter a movie name no. " + (counter+1));
+                movieName = Console.ReadLine();// read in name
+                Console.WriteLine("NAME: ");// write name to screen
+                Console.WriteLine(movieName);
+                // asks user for movie year and echo's response
+                System.Console.WriteLine("Please enter a movie year no. " + (counter + 1));
+                movieYear = int.Parse(Console.ReadLine());
+                Console.WriteLine("YEAR: " + movieYear);
+                // asks user for movie rating and echo's response
+                System.Console.WriteLine("Please enter a movie rating no. " + (counter + 1));
+                movieRating = Console.ReadLine();// read in name
+                Console.WriteLine("RATING: ");// write name to screen
+                Console.WriteLine(movieRating);
+                counter++;
+            } while (counter < 3);
+        }
+    }
+}
